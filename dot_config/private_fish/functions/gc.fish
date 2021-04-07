cbe2b528c1b021eb29eb7bc8d8be454a387f04bd
Subaru Shoji <shoji.subaru@gmail.com>
2021-04-07 19:56:40 +0900

    fix: gcで出るブランチの順番を変更

--- dot_config/private_fish/functions/gc.fish
+++ dot_config/private_fish/functions/gc.fish
@@ -1,4 +1,4 @@
 # Defined via `source`
 function gc --wraps='git branch | fzf | xargs git switch' --description 'alias gc=git branch | fzf | xargs git switch'
-  git branch | fzf | xargs git switch $argv; 
+  git branch | sort -r | fzf | xargs -I{} git switch {} $argv;
 end
