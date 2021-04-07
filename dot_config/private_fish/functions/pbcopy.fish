99c80f06cf881649050abb8895ad7c40cbb77f84
Subaru Shoji <shoji.subaru@gmail.com>
2021-04-07 14:27:04 +0900

    fix: fix fish functions.

--- dot_config/private_fish/functions/pbcopy.fish
+++ dot_config/private_fish/functions/pbcopy.fish
@@ -1,4 +1,9 @@
 # Defined in - @ line 1
 function pbcopy --wraps='xclip -selection clipboard' --description 'alias pbcopy=xclip -selection clipboard'
-  xclip -selection clipboard $argv;
+  switch (uname)
+  case Darwin
+      /usr/bin/pbcopy
+  case '*'
+      xclip -selection clipboard $argv;
+  end
 end
