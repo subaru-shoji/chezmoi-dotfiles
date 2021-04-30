f65d5985d134d1d9173698581f0a55e25120b78c
Subaru Shoji <ez.do.subi@gmail.com>
2021-05-01 01:22:15 +0900

    feat: fis path on ce.fish.

--- dot_config/private_fish/functions/ce.fish
+++ dot_config/private_fish/functions/ce.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function ce --wraps='chezmoi managed -i f | fzf | xargs chezmoi edit --apply' --description 'alias ce=chezmoi managed -i f | fzf | xargs chezmoi edit --apply'
-  chezmoi managed -i files | fzf | xargs chezmoi edit --apply $argv;
+  chezmoi managed -i files | fzf | xargs -I{} chezmoi edit --apply ~/{};
 end
