8954e6fde35a2b8a415a1205ca16a957a63c2eb8
Subaru Shoji <shoji.subaru@gmail.com>
2021-07-31 16:57:36 +0900

    feat: skk, git, keybindings.

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -3,212 +3,128 @@ local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({"git", "clone", "https://github.com/wbthomason/packer.nvim", install_path})
-  execute "packadd packer.nvim"
+    fn.system({
+        "git", "clone", "https://github.com/wbthomason/packer.nvim",
+        install_path
+    })
+    execute "packadd packer.nvim"
 end
 
-vim.cmd [[packadd packer.nvim]]
-require("packer").startup(
-  function(use)
+require("packer").startup(function(use)
     use "wbthomason/packer.nvim"
     use "svermeulen/vimpeccable"
     use "norcalli/nvim_utils"
     use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
+    use {"hoob3rt/lualine.nvim", requires = {"kyazdani42/nvim-web-devicons"}}
     use {
-      "hoob3rt/lualine.nvim",
-      requires = {
-        "kyazdani42/nvim-web-devicons"
-      }
+        "nvim-telescope/telescope.nvim",
+        requires = {{"nvim-lua/popup.nvim"}, {"nvim-lua/plenary.nvim"}}
     }
     use {
-      "nvim-telescope/telescope.nvim",
-      requires = {{"nvim-lua/popup.nvim"}, {"nvim-lua/plenary.nvim"}}
+        "kyazdani42/nvim-tree.lua",
+        requires = {"kyazdani42/nvim-web-devicons"}
     }
     use {
-      "kyazdani42/nvim-tree.lua",
-      requires = {
-        "kyazdani42/nvim-web-devicons"
-      }
-    }
-    use {
-      "lambdalisue/fern.vim",
-      setup = function()
-        vim.g["fern#renderer"] = "nerdfont"
-      end
+
+        "lambdalisue/fern.vim",
+        setup = function() vim.g["fern#renderer"] = "nerdfont" end
     }
     use "lambdalisue/nerdfont.vim"
     use "lambdalisue/glyph-palette.vim"
     use "lambdalisue/fern-renderer-nerdfont.vim"
     use "lambdalisue/fern-git-status.vim"
     use "lambdalisue/fern-hijack.vim"
-
+    use {"neovim/nvim-lspconfig"}
     use {
-      "mhartington/formatter.nvim",
-      config = function()
-        require("formatter").setup {
-          logging = false,
-          filetype = {
-            lua = {
-              -- luafmt
-              function()
-                return {
-                  exe = "luafmt",
-                  args = {"--indent-count", 2, "--stdin"},
-                  stdin = true
-                }
-              end
-            }
-          }
-        }
-
-        vim.api.nvim_exec(
-          [[
-						augroup FormatAutogroup
-							autocmd!
-							autocmd BufWritePost *.lua FormatWrite
-						augroup END
-					]],
-          true
-        )
-      end
-    }
-    use {
-      "neovim/nvim-lspconfig"
-    }
-    use {
-      "kabouzeid/nvim-lspinstall",
-      config = function()
-        require "lspinstall".setup() -- important
-
-        local function setup_servers()
-          require "lspinstall".setup()
-          local servers = require "lspinstall".installed_servers()
-          for _, server in pairs(servers) do
-            require "lspconfig"[server].setup {}
-          end
+        "tjdevries/nlua.nvim",
+        config = function()
+            require('nlua.lsp.nvim').setup(require('lspconfig'), {})
+            vim.cmd [[autocmd BufWritePost *.lua silent lua require'nlua'.format_file()]]
         end
+    }
 
-        setup_servers()
-
-        -- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
-        require "lspinstall".post_install_hook = function()
-          setup_servers() -- reload installed servers
-          vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
+    use {"tyru/eskk.vim"}
+    use {
+        "glepnir/lspsaga.nvim",
+        config = function()
+            local saga = require "lspsaga"
+            saga.init_lsp_saga()
         end
+    }
+    use {
+        "kosayoda/nvim-lightbulb",
+        config = function()
+            vim.cmd [[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()]]
+        end
+    }
+    use {
+        "onsails/lspkind-nvim",
+        config = function() require("lspkind").init() end
+    }
+    use {
+        "nvim-lua/completion-nvim",
+        config = function()
+            vim.g.completion_enable_auto_popup = 0
+            vim.g.completion_sorting = "none"
 
-        local lspconfig = require "lspconfig"
-        lspconfig.lua.setup {
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = {"vim"}
-              }
-            }
-          }
-        }
-      end
-    }
-    use {
-      "glepnir/lspsaga.nvim",
-      config = function()
-        local saga = require "lspsaga"
-        saga.init_lsp_saga()
-      end
-    }
-    use {
-      "kosayoda/nvim-lightbulb",
-      config = function()
-        vim.cmd [[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()]]
-      end
-    }
-    use {
-      "onsails/lspkind-nvim",
-      config = function()
-        require("lspkind").init()
-      end
-    }
-    use {
-      "nvim-lua/completion-nvim",
-      config = function()
-        vim.g.completion_enable_auto_popup = 0
-        vim.g.completion_sorting = "none"
+            vim.cmd [[autocmd BufEnter * lua require'completion'.on_attach()]]
 
-        vim.cmd [[autocmd BufEnter * lua require'completion'.on_attach()]]
-
-        vim.nvim_exec(
-          [[
+            vim.api.nvim_exec([[
 					augroup CompletionTriggerCharacter
 							autocmd!
 							autocmd BufEnter * let completion_enable_auto_popup = 1
 							autocmd BufEnter *.lua let completion_enable_auto_popup = 0
 					augroup end
-				]],
-          false
-        )
-      end
+				]], false)
+        end
     }
+    use 'famiu/bufdelete.nvim'
+
     use {
-      "folke/which-key.nvim",
-      config = function()
-        require("which-key").setup {}
-      end
+        "folke/which-key.nvim",
+        config = function() require("which-key").setup {} end
     }
     use "marko-cerovac/material.nvim"
     use "euclidianAce/BetterLua.vim"
     use {"rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"}}
     use {
-      "windwp/nvim-autopairs",
-      config = function()
-        require("nvim-autopairs").setup()
-      end
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup() end
     }
     use "b3nj5m1n/kommentary"
     use "tversteeg/registers.nvim"
     use {
-      "TimUntersberger/neogit",
-      config = function()
-        require("neogit").setup {
-          integrations = {
-            diffview = true
-          }
-        }
-      end
+        "TimUntersberger/neogit",
+        config = function()
+            require("neogit").setup {integrations = {diffview = true}}
+        end
     }
-    use "sindrets/diffview.nvim"
-    use "f-person/git-blame.nvim"
-    use "yamatsum/nvim-cursorline"
+    use {'iberianpig/tig-explorer.vim', requires = {'rbgrouleff/bclose.vim'}}
     use {
-      "phaazon/hop.nvim",
-      as = "hop",
-      config = function()
-        -- you can configure Hop the way you like here; see :h hop-config
-        require "hop".setup {keys = "etovxqpdygfblzhckisuran"}
-      end
+
+        "phaazon/hop.nvim",
+        as = "hop",
+        config = function()
+            -- you can configure Hop the way you like here; see :h hop-config
+            require"hop".setup {keys = "etovxqpdygfblzhckisuran"}
+        end
     }
     use {
-      "akinsho/nvim-bufferline.lua",
-      requires = "kyazdani42/nvim-web-devicons",
-      config = function()
-        require("bufferline").setup {}
-      end
+        "akinsho/nvim-bufferline.lua",
+        requires = "kyazdani42/nvim-web-devicons",
+        config = function() require("bufferline").setup {} end
     }
     use "simrat39/symbols-outline.nvim"
     use "mg979/vim-visual-multi"
     use "yuki-yano/fern-preview.vim"
-    use "danilamihailov/beacon.nvim"
-  end
-)
+end)
 
 vim.cmd("set guicursor=")
 vim.o.termguicolors = true
 
 vim.o.pumblend = 15
 require("material").set()
-require("lualine").setup {
-  options = {
-    theme = "material-nvim"
-  }
-}
+require("lualine").setup {options = {theme = "material-nvim"}}
 
 local vimp = require("vimp")
 
@@ -223,7 +139,8 @@ vim.o.shiftwidth = vim.o.tabstop
 vim.g.mapleader = " "
 
 vim.wo.number = true
-vim.wo.cursorcolumn = true
+-- vim.wo.cursorcolumn = true
+vim.wo.cursorline = true
 
 vimp.imap("<tab>", "<Plug>(completion_smart_tab)")
 vimp.imap("<s-tab>", "<Plug>(completion_smart_s_tab)")
@@ -237,6 +154,7 @@ vim.o.shortmess = vim.o.shortmess .. "c"
 vim.g.timeoutlen = 10
 
 vimp.imap("qq", "<esc>")
+vimp.imap("<c-q>", "<esc>")
 
 vimp.imap({"silent"}, "<c-a>", "<c-o>I")
 vimp.imap({"silent"}, "<c-e>", "<c-o>A")
@@ -268,15 +186,12 @@ vimp.imap("<s-down>", "<esc>v<down>")
 vimp.imap("<s-left>", "<esc>v<left>")
 vimp.imap("<s-right>", "<esc>v<right>")
 
-vim.api.nvim_exec(
-  [[
+vim.api.nvim_exec([[
   augroup Packer
     autocmd!
     autocmd BufEnter * map <buffer> <silent> <s-k> :BufferLineCycleNext<cr>
   augroup end
-]],
-  false
-)
+]], false)
 vimp.nnoremap({"silent"}, "J", ":BufferLineCyclePrev<cr>")
 vimp.nnoremap("L", "<c-w>w")
 vimp.nnoremap("W", "<c-w>w")
@@ -284,169 +199,95 @@ vimp.nnoremap("W", "<c-w>w")
 local wk = require("which-key")
 local telescope = require("telescope.builtin")
 
-wk.register(
-  {
+wk.register({
     D = {
-      function()
-        require("lspsaga.hover").render_hover_doc()
-      end,
-      "definition"
+        function() require("lspsaga.hover").render_hover_doc() end, "definition"
     },
-    H = {
-      function()
-        require("lspsaga.hover").render_hover_doc()
-      end,
-      "hover"
-    },
-    R = {
-      function()
-        require "lspsaga.provider".lsp_finder()
-      end,
-      "references"
-    },
-    f = {
-      function()
-        require "hop".hint_words()
-      end,
-      "hop"
-    },
-    F = {
-      ":HopChar1<cr>",
-      "hop char1"
-    }
-  },
-  {}
-)
+    H = {function() require("lspsaga.hover").render_hover_doc() end, "hover"},
+    R = {function() require"lspsaga.provider".lsp_finder() end, "references"},
+    f = {function() require"hop".hint_words() end, "hop"},
+    F = {":HopChar1<cr>", "hop char1"}
+}, {})
 
 vimp.inoremap("<c-f>", [[<c-o><cmd>lua require "hop".hint_words()<cr>]])
 
-local find_files_action = {
-  function()
-    telescope.find_files()
-  end,
-  "find file"
+local find_files_action = {function() telescope.find_files() end, "find file"}
+local find_match_word_action = {
+    function() require("telescope.builtin").live_grep() end, "find match word"
 }
 
-wk.register(
-  {
+wk.register({
     [" "] = find_files_action,
+    ["/"] = find_match_word_action,
     f = {
-      name = "file",
-      f = find_files_action,
-      r = {
-        function()
-          require("telescope.builtin").oldfiles()
-        end,
-        "find recent files"
-      },
-      w = {
-        function()
-          require("telescope.builtin").live_grep()
-        end,
-        "find match word"
-      },
-      c = {
-        function()
-          telescope.commands()
-        end,
-        "find command"
-      }
+        name = "file",
+        f = find_files_action,
+        r = {
+            function() require("telescope.builtin").oldfiles() end,
+            "find recent files"
+        },
+        g = find_match_word_action,
+        s = find_match_word_action,
+        ["/"] = find_match_word_action,
+        w = find_match_word_action,
+        c = {function() telescope.commands() end, "find command"}
     },
     q = {
-      a = {":qa<cr>", "quit all"},
-      w = {"<c-w>c", "quit window"},
-      q = {":bdelete<cr>", "quit buffer"}
+        a = {
+            function()
+                if (vim.fn.confirm("Quit all?", "Yes\nNo") == 1) then
+                    vim.cmd(":qa")
+                end
+            end, "quit all"
+        },
+        w = {"<c-w>c", "quit window"},
+        q = {":Bdelete<cr>", "quit buffer"},
+        b = {":Bdelete<cr>", "quit buffer"}
     },
     g = {
-      s = {
-        function()
-          require("neogit").open({kind = "split"})
-        end,
-        "neogit status"
-      },
-      c = {
-        function()
-          require("neogit").open({"commit"})
-        end,
-        "neogit commit"
-      },
-      b = {
-        function()
-          require("neogit").open({"branch"})
-        end,
-        "neogit branch"
-      },
-      l = {
-        function()
-          require("neogit").open({"log"})
-        end,
-        "neogit log"
-      }
+        s = {
+            function() require("neogit").open({kind = "split"}) end,
+            "neogit status"
+        },
+        c = {function() require("neogit").open({"commit"}) end, "neogit commit"},
+        b = {function() require("neogit").open({"branch"}) end, "neogit branch"},
+        l = {function() require("neogit").open({"log"}) end, "neogit log"},
+        t = {":Tig<cr>", "tig"},
+        g = {":Tig<cr>", "tig"}
     },
     y = {'"+y', "yank to clipboard"},
     e = {
-      e = {
-        ":Fern . -reveal=%<cr>",
-        "file explorer"
-      },
-      t = {
-        ":Fern . -drawer -reveal=% -toggle<cr>",
-        "file tree"
-      }
+        e = {":Fern . -reveal=%<cr>", "file explorer"},
+        t = {":Fern . -drawer -reveal=% -toggle<cr>", "file tree"}
     },
     b = {
-      f = {
-        ":NvimTreeToggle<cr>",
-        "file-tree bar"
-      },
-      s = {":SymbolsOutline<cr>", "symbols-outline bar"}
+        f = {":NvimTreeToggle<cr>", "file-tree bar"},
+        s = {":SymbolsOutline<cr>", "symbols-outline bar"}
     },
     ["."] = {
-      function()
-        require("lspsaga.codeaction").code_action()
-      end,
-      "lsp action"
+        function() require("lspsaga.codeaction").code_action() end, "lsp action"
     }
-  },
-  {prefix = "<leader>"}
-)
+}, {prefix = "<leader>"})
 
-wk.register(
-  {
-    d = {
-      function()
-        vim.lsp.buf.definition()
-      end,
-      "go to definition"
-    },
+wk.register({
+    d = {function() vim.lsp.buf.definition() end, "go to definition"},
     r = {
-      function()
-        require "lspsaga.provider".lsp_finder()
-      end,
-      "show reference"
+        function() require"lspsaga.provider".lsp_finder() end, "show reference"
     },
     k = {"gg", "go to top"},
     j = {"G", "go to bottom"}
-  },
-  {prefix = "g"}
-)
+}, {prefix = "g"})
 
-vim.api.nvim_exec(
-  [[
+vim.api.nvim_exec([[
 		augroup fern-settings
 		autocmd!
 		autocmd FileType fern nmap <silent> <buffer> H <Plug>(fern-action-preview:toggle)
 		augroup END
-	]],
-  false
-)
+	]], false)
 
-vim.api.nvim_exec(
-  [[
+vim.api.nvim_exec([[
 		if executable('fcitx')
 			 autocmd InsertLeave * :call system('fcitx-remote -c')
 			 autocmd CmdlineLeave * :call system('fcitx-remote -c')
 		endif
-	]],
-  false
-)
+	]], false)
