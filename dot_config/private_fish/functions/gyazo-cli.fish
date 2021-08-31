29e9868e1170d792cbb16f6344cffc4b73ed1b5f
Subaru Shoji <ez.do.subi@gmail.com>
2021-08-31 12:07:39 +0900

    feat: delete gyazo-cli fish script.

--- dot_config/private_fish/functions/gyazo-cli.fish
+++
@@ -1,4 +0,0 @@
-function gyazo-cli --wraps=curl --description 'alias gyazo-cli=curl'
-  curl https://upload.gyazo.com/api/upload -F "access_token=$GYAZO_ACCESS_TOKEN" \
-  -F "imagedata=@$argv" | jq -r ".url"; 
-end
