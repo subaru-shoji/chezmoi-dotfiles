c4688cde9e2cbec9643e2986c97ce5bdee637af9
東海林昴 <shoujisubaru@shoujisubarus-MacBook-Air.local>
2021-09-30 01:21:43 +0900

    fix: pathが汎用的でなかったのを修正

--- dot_config/private_fish/functions/cv.fish
+++ dot_config/private_fish/functions/cv.fish
@@ -1,3 +1,3 @@
 function cv --wraps='chezmoi edit --apply /home/simple-web-system/.config/nvim/init.lua' --description 'alias cv=chezmoi edit --apply /home/simple-web-system/.config/nvim/init.lua'
-  chezmoi edit --apply /home/simple-web-system/.config/nvim/init.lua $argv; 
+  chezmoi edit --apply ~/.config/nvim/init.lua $argv; 
 end
