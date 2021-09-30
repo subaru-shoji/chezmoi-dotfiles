cd82fe6d9f060070751b612f6e58d278ec66eba0
Subaru Shoji <shoji.subaru@gmail.com>
2021-09-30 16:48:02 +0900

    feat: fcitx用にswitchスクリプトを修正

---
+++ dot_config/private_fish/functions/fcitx-en-layout.fish
@@ -0,0 +1,3 @@
+function fcitx-en-layout --wraps='fcitx5-remote -g en_layout' --description 'alias fcitx-en-layout=fcitx5-remote -g en_layout'
+  fcitx5-remote -g en_layout $argv; 
+end
---
+++ dot_config/private_fish/functions/fcitx-jp-layout.fish
@@ -0,0 +1,3 @@
+function fcitx-jp-layout --wraps='fcitx5-remote -g jp_layout' --description 'alias fcitx-jp-layout=fcitx5-remote -g jp_layout'
+  fcitx5-remote -g jp_layout $argv; 
+end
