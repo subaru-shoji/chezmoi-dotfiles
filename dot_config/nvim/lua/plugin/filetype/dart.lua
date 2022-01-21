57e3d44f8a50efb0d1dd608edcfc031ce2e045ee
Subaru Shoji <shoji.subaru@gmail.com>
2022-01-21 22:42:18 +0900

    feat: add sdk path.

--- dot_config/nvim/lua/plugin/filetype/dart.lua
+++ dot_config/nvim/lua/plugin/filetype/dart.lua
@@ -2,6 +2,10 @@ return {
     {
         "akinsho/flutter-tools.nvim",
         requires = {"nvim-lua/plenary.nvim"},
-        config = function() require("flutter-tools").setup {} end
+        config = function()
+            require("flutter-tools").setup {
+                flutter_lookup_cmd = "asdf where flutter"
+            }
+        end
     }
 }
