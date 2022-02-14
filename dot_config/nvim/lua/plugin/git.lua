51b5414a02fcfbafcbaca1413f51a1834a3c8f1a
Subaru Shoji <shoji.subaru@gmail.com>
2022-02-14 19:48:17 +0900

    feat: add git util.

--- dot_config/nvim/lua/plugin/git.lua
+++ dot_config/nvim/lua/plugin/git.lua
@@ -1,11 +1,14 @@
 return {
-	{
-		"TimUntersberger/neogit",
-		config = function()
-			require("neogit").setup({ integrations = { diffview = true } })
-		end,
-	},
-	{ "tyru/open-browser-github.vim", requires = "tyru/open-browser.vim" },
-	{ "iberianpig/tig-explorer.vim", requires = { "rbgrouleff/bclose.vim" } },
-	{ "emmanueltouzery/agitator.nvim" },
+    {
+        "TimUntersberger/neogit",
+        config = function()
+            require("neogit").setup({integrations = {diffview = true}})
+        end
+    }, {"tyru/open-browser-github.vim", requires = "tyru/open-browser.vim"},
+    {"iberianpig/tig-explorer.vim", requires = {"rbgrouleff/bclose.vim"}},
+    {"emmanueltouzery/agitator.nvim"}, {
+        "lewis6991/gitsigns.nvim",
+        requires = {'nvim-lua/plenary.nvim'},
+        config = function() require('gitsigns').setup() end
+    }
 }
