eed7b6086c8a2e40f7e33d2b6f3024c7da24a85f
Subaru Shoji <ez.do.subi@gmail.com>
2022-02-15 00:53:10 +0900

    feat: use storm tokyo night.

--- dot_config/nvim/lua/plugin/color.lua
+++ dot_config/nvim/lua/plugin/color.lua
@@ -3,7 +3,7 @@ return {
         "folke/tokyonight.nvim",
         setup = function() vim.o.termguicolors = true end,
         config = function()
-            vim.g.tokyonight_style = "night"
+            vim.g.tokyonight_style = "storm"
             vim.g.tokyonight_italic_functions = true
             vim.g.tokyonight_sidebars = {}
             vim.g.tokyonight_hide_inactive_statusline = true;
