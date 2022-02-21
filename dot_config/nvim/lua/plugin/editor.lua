7ca510738eded876efdbb210745aa60c6ada8000
Subaru Shoji <shoji.subaru@gmail.com>
2022-02-21 14:14:41 +0900

    feat: add neoclip.

--- dot_config/nvim/lua/plugin/editor.lua
+++ dot_config/nvim/lua/plugin/editor.lua
@@ -42,5 +42,12 @@ return {
             })
 
         end
-    }, {"famiu/bufdelete.nvim"}
+    }, {"famiu/bufdelete.nvim"}, {
+        "AckslD/nvim-neoclip.lua",
+        requires = {'nvim-telescope/telescope.nvim'},
+        config = function()
+            require('neoclip').setup()
+            require('telescope').load_extension('neoclip')
+        end
+    }
 }
