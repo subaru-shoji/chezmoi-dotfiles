8f672e9906405c956c067cef96670ce58dba32d1
Subaru Shoji <shoji.subaru@gmail.com>
2022-03-15 01:07:29 +0900

    feat: use fzf-tmux on ce.

--- dot_config/private_fish/functions/ce.fish
+++ dot_config/private_fish/functions/ce.fish
@@ -1,6 +1,6 @@
 # Defined in - @ line 1
 function ce --wraps='chezmoi managed -i f | fzf | xargs chezmoi edit --apply' --description 'alias ce=chezmoi managed -i f | fzf | xargs chezmoi edit --apply'
-	set SEARCH_RESULT (chezmoi managed --include=files | fzf)
+	set SEARCH_RESULT (chezmoi managed --include=files | fzf-tmux -p)
 	if test $status -eq 0
 		chezmoi edit --apply $HOME/$SEARCH_RESULT
 	end
