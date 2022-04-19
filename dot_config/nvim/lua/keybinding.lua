e151113211f9c1bf9dd70a748f4b9fb0de5780fc
Subaru Shoji <shoji.subaru@gmail.com>
2022-04-19 23:55:05 +0900

    feat: change quit key.

--- dot_config/nvim/lua/keybinding.lua
+++ dot_config/nvim/lua/keybinding.lua
@@ -94,26 +94,23 @@ wk.register({
         t = {"<cmd>TroubleToggle<cr>", "trouble bar"}
     },
     q = {
-        name = "quit",
-        a = {
-            function()
-                if vim.fn.confirm("Quit all?", "Yes\nNo") == 1 then
-                    vim.cmd("qa")
-                end
-            end, "quit all"
-        },
-        q = {
-            function()
-                local file_type_list = {"NvimTree", "qf", "Trouble"}
+        function()
+            local file_type_list = {"NvimTree", "qf", "Trouble"}
 
-                if require("util").has_value(file_type_list, vim.bo.filetype) then
-                    vim.api.nvim_command "wincmd c"
-                else
-                    require("bufdelete").bufdelete(0, false)
-                end
+            if require("util").has_value(file_type_list, vim.bo.filetype) then
+                vim.api.nvim_command "wincmd c"
+            else
+                require("bufdelete").bufdelete(0, false)
+            end
 
-            end, "smart quit buffer"
-        }
+        end, "smart quit buffer"
+    },
+    Q = {
+        function()
+            if vim.fn.confirm("Quit all?", "Yes\nNo") == 1 then
+                vim.cmd("qa")
+            end
+        end, "quit all"
     },
     g = {
         name = "git",
