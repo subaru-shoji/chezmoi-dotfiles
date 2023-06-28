401ba96ad6298502314b3baeead40ac841c94540
Subaru Shoji <shoji.subaru@gmail.com>
2023-06-28 13:39:21 +0900

    Update wezterm.lua to add mac setting.

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,7 +1,12 @@
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
+
+local use_ime = false
+if wezterm.target_triple == "aarch64-apple-darwin" then
+	use_ime = true
+end
 
 return {
-    default_prog = {"zsh", "-lc", "tmux"},
-    font = wezterm.font("FiraCode Nerd Font Mono"),
-    use_ime = false
+	default_prog = { "zsh", "-lc", "tmux" },
+	font = wezterm.font("FiraCode Nerd Font Mono"),
+	use_ime = use_ime,
 }
