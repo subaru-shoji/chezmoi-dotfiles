952fd58ca9749bc7531e1379601423808080b6ac
Subaru Shoji <shoji.subaru@gmail.com>
2023-06-28 15:26:07 +0900

    feat: add mac config to fish.

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,7 +1,11 @@
 source ~/.asdf/asdf.fish
 source ~/.asdf/completions/asdf.fish
 
+if test (uname) = "Darwin"
+	source ~/.config/fish/os/mac_config.fish
+end
+
 starship init fish | source
 zoxide init fish | source
 direnv hook fish | source
-
+  
---
+++ dot_config/private_fish/os/mac_config.fish
@@ -0,0 +1,21 @@
+# Defined in - @ line 1
+function android_studio 
+	open -a "Android Studio" $argv;
+end
+
+function AndroidStudio
+  open -a "Android Studio" $argv;
+end
+
+function subl
+  open -a "Sublime Text" $argv;
+end
+
+function sublimerge
+  open -a "Sublime Merge" $argv;
+end
+
+function gittyup
+  open -a "Gittyup" $argv;
+end
+
