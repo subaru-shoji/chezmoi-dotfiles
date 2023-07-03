fe368297f4ea77d6657a2154cedf2db9423bf12b
Subaru Shoji <shoji.subaru@gmail.com>
2023-07-03 23:24:53 +0900

    feat: change gc branch order.

--- dot_config/private_fish/functions/gc.fish
+++ dot_config/private_fish/functions/gc.fish
@@ -1,4 +1,4 @@
 # Defined via `source`
 function gc --wraps='git branch | fzf | xargs git switch' --description 'alias gc=git branch | fzf | xargs git switch'
-  git branch | sort -r | fzf | xargs -I{} git switch {} $argv;
+  git branch --sort=-committerdate | fzf | xargs -I{} git switch {} $argv;
 end
