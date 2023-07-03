89d982ae159a82370f694f2f71dbfa64ecab7e56
Subaru Shoji <ez.do.subi@gmail.com>
2023-07-03 17:24:26 +0900

    fix: fix conflicting indention between treesitter and lsp.

--- dot_config/nvim/lua/plugin/highlight.lua
+++ dot_config/nvim/lua/plugin/highlight.lua
@@ -1,19 +1,17 @@
 return {
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',
-        config = function()
-            require'nvim-treesitter.configs'.setup {
-                ensure_installed = "all",
-                ignore_install = {"phpdoc"},
-                sync_install = true,
-                highlight = {enable = true},
-                indent = {enable = true}
-
-            }
-            vim.o.foldmethod = 'expr'
-            vim.o.foldexpr = 'nvim_treesitter#foldexpr'
-        end
-    }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = "all",
+				ignore_install = { "phpdoc" },
+				sync_install = true,
+				highlight = { enable = true },
+				indent = { enable = true, disable = { "dart" } },
+			})
+			vim.o.foldmethod = "expr"
+			vim.o.foldexpr = "nvim_treesitter#foldexpr"
+		end,
+	},
 }
-
