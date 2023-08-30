b1e75795f9b9ec92a6b9c26db2bbeb87b57780a5
Subaru Shoji <shoji.subaru@gmail.com>
2023-08-30 22:12:42 +0900

    feat: add nvim-tree key config.

--- dot_config/nvim/lua/plugin/sidebar.lua
+++ dot_config/nvim/lua/plugin/sidebar.lua
@@ -50,7 +50,21 @@ return {
 		lazy = true,
 		cmd = { "NvimTreeOpen" },
 		config = function()
+			local function my_on_attach(bufnr)
+				local api = require("nvim-tree.api")
+
+				local function opts(desc)
+					return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+				end
+
+				-- default mappings
+				api.config.mappings.default_on_attach(bufnr)
+
+				-- custom mappings
+				vim.keymap.set("n", "l", api.node.open.edit, opts("edit"))
+			end
 			require("nvim-tree").setup({
+				on_attach = my_on_attach,
 				diagnostics = { enable = true },
 			})
 		end,
