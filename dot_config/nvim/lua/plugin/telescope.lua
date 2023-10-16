28eee2bf76075fcc9ded6d01b94aa9a00c0d2b3c
Subaru Shoji <ez.do.subi@gmail.com>
2023-10-17 02:29:05 +0900

    feat: update telescope.

--- dot_config/nvim/lua/plugin/telescope.lua
+++ dot_config/nvim/lua/plugin/telescope.lua
@@ -1,9 +1,8 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.1",
+		tag = "0.1.4",
 		dependencies = {
-			{ "nvim-lua/popup.nvim" },
 			{ "nvim-lua/plenary.nvim" },
 		},
 		config = function()
