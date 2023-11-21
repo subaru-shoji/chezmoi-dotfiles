3056e09629fb669cbb049cd8b089acd6534581e9
Subaru Shoji <shoji.subaru@gmail.com>
2023-11-21 15:35:22 +0900

    feat: change wezterm config to use it as main terminal.

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -7,6 +7,19 @@ end
 
 return {
 	default_prog = { "zsh", "-lc", "tmux" },
-	font = wezterm.font("FiraCode Nerd Font Mono"),
+	-- font = wezterm.font("FiraCode Nerd Font Mono"),
+	font = wezterm.font("Hack Nerd Font Mono"),
+	font_size = 13,
 	use_ime = use_ime,
+	window_background_opacity = 0.87,
+	hide_tab_bar_if_only_one_tab = true,
+	keys = {
+		-- Turn off the default CMD-m Hide action, allowing CMD-m to
+		-- be potentially recognized and handled by the tab
+		{
+			key = "Enter",
+			mods = "ALT",
+			action = wezterm.action.DisableDefaultAssignment,
+		},
+	},
 }
