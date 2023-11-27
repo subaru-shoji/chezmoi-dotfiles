41d271072abea315d0c9a790ff646359ac3a510a
Subaru Shoji <ez.do.subi@gmail.com>
2023-11-27 11:55:23 +0900

    feat: always enable ime on wezterm.

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,16 +1,11 @@
 local wezterm = require("wezterm")
 
-local use_ime = false
-if wezterm.target_triple == "aarch64-apple-darwin" then
-	use_ime = true
-end
-
 return {
 	default_prog = { "zsh", "-lc", "tmux" },
 	-- font = wezterm.font("FiraCode Nerd Font Mono"),
 	font = wezterm.font("Hack Nerd Font Mono"),
 	font_size = 13,
-	use_ime = use_ime,
+	use_ime = true,
 	window_background_opacity = 0.87,
 	hide_tab_bar_if_only_one_tab = true,
 	keys = {
