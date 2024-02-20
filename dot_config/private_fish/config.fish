30b111169afe5ad5c22f2c840deb15195bd19dd1
Subaru Shoji <shoji.subaru@gmail.com>
2024-02-20 17:23:44 +0900

    feat: add interactive check.

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,15 +1,19 @@
-source ~/.asdf/asdf.fish
-source ~/.asdf/completions/asdf.fish
-
 if test (uname) = "Darwin"
 	source ~/.config/fish/os/mac_config.fish
 end
 
-starship init fish | source
-zoxide init fish | source
-direnv hook fish | source
-  
 fish_add_path ~/bin
-fish_add_path (yarn global bin)
+fish_add_path $YARN_PATH
 fish_add_path ~/.dotnet/tools
 fish_add_path ~/Applications
+
+source ~/.asdf/asdf.fish
+
+if status is-interactive
+	source ~/.asdf/completions/asdf.fish
+
+	zoxide init fish | source
+	direnv hook fish | source
+	starship init fish | source
+end
+  
