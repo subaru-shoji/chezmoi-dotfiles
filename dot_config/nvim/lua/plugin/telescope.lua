27b6aef6371cded8618aeda8d566b8ece0ef98b8
Subaru Shoji <shoji.subaru@gmail.com>
2024-03-01 14:31:50 +0900

    feat: add ctrl u on telescope.

--- dot_config/nvim/lua/plugin/telescope.lua
+++ dot_config/nvim/lua/plugin/telescope.lua
@@ -32,6 +32,7 @@ return {
 							["<C-k>"] = actions.move_selection_previous,
 							["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
 							["<esc>"] = actions.close,
+							["<C-u>"] = false,
 						},
 					},
 				},
