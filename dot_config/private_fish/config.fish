267060d98f50c23b337d07d0dc008600f1bcded3
Subaru Shoji <shoji.subaru@gmail.com>
2024-07-03 22:54:53 +0900

    feat: add cargo and pnpm.

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -7,7 +7,11 @@ fish_add_path $YARN_PATH
 fish_add_path ~/.dotnet/tools
 fish_add_path ~/Applications
 fish_add_path $HOME/.local/bin/
+fish_add_path $HOME/.cargo/bin/
 fish_add_path /usr/share/applications/
+
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
+fish_add_path $PNPM_HOME
 
 source ~/.asdf/asdf.fish
 
