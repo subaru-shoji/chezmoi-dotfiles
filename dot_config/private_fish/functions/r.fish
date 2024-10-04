284ad3fa839f5d3d4e9f63c2b442a0f74b618f6c
Subaru Shoji <subarushoji@ip-10-241-0-128.ap-northeast-1.compute.internal>
2024-10-04 17:49:41 +0900

    feat: change tools.

--- dot_config/private_fish/functions/r.fish
+++ dot_config/private_fish/functions/r.fish
@@ -1,9 +1,9 @@
 # Defined in - @ line 1
 function r
-  set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
 	yazi $argv --cwd-file="$tmp"
 	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		cd -- "$cwd"
+		builtin cd -- "$cwd"
 	end
 	rm -f -- "$tmp"
 end
--- dot_config/private_fish/functions/t.fish
+++ dot_config/private_fish/functions/t.fish
@@ -1,4 +1,5 @@
 # Defined in - @ line 1
 function t --wraps=tig --description 'alias t=tig'
-  env TERM=xterm-256color tig  $argv;
+  lazygit
+  # env TERM=xterm-256color tig  $argv;
 end
