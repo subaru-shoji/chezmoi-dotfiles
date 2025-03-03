b3a31824b3440d21221b53f752d7136f671851b2
Subaru Shoji <shoji.subaru@gmail.com>
2025-03-04 02:12:02 +0900

    update configs.

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -13,11 +13,21 @@ fish_add_path /usr/share/applications/
 set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 fish_add_path $PNPM_HOME
 
-source ~/.asdf/asdf.fish
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
 
 if test "$TERM" = "tmux-256color"; or test -n "$ZELLIJ"
-	source ~/.asdf/completions/asdf.fish
-
 	zoxide init fish | source
 	direnv hook fish | source
 	starship init fish | source
---
+++ dot_config/private_fish/functions/vc.fish
@@ -0,0 +1,4 @@
+function vc --wraps=code-insiders --description 'alias vc=code-insiders'
+  code-insiders $argv
+        
+end
