22beec9f5c1ec8d2757badcc387349ce6effc3f2
Subaru Shoji <ez.do.subi@gmail.com>
2025-09-12 01:36:07 +0900

    update

--- dot_config/nvim/lua/plugin/language_support/highlight.lua
+++ dot_config/nvim/lua/plugin/language_support/highlight.lua
@@ -1,20 +1,6 @@
 return {
 	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		lazy = true,
-		event = { "BufReadPost", "BufNewFile" },
-		cmd = { "TSUpdateSync" },
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = "all",
-				ignore_install = { "phpdoc" },
-				sync_install = true,
-				highlight = { enable = true },
-				indent = { enable = true, disable = { "dart" } },
-			})
-			vim.o.foldmethod = "expr"
-			vim.o.foldexpr = "nvim_treesitter#foldexpr"
-		end,
-	},
+		"nvim-treesitter/nvim-treesitter", 
+		build = ":TSUpdate"
+	}
 }
