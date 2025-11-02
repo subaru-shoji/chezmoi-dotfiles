1bb9f11acdc92f2a629ed075045e637db8220770
Subaru Shoji <ez.do.subi@gmail.com>
2025-11-02 20:50:24 +0900

    feat: use typescript-tools.

--- dot_config/nvim/lua/exact_plugin/lsp/nvim-lspconfig.lua
+++ dot_config/nvim/lua/exact_plugin/lsp/nvim-lspconfig.lua
@@ -7,7 +7,7 @@ return {
 		lazy = true,
 		event = { "BufReadPre", "BufNewFile" },
 		config = function()
-			vim.lsp.enable({ "lua_ls", "ts_ls", "biome", "dartls", "pyright", "ruff_lsp" })
+			vim.lsp.enable({ "lua_ls", "biome", "dartls", "pyright", "ruff_lsp" })
 		end,
 	},
 }
