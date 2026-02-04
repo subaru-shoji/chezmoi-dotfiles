ab85a663a5fd417cd64a6bc80c4a24e7c8fe9b14
Subaru Shoji <32668181+shoji-sws@users.noreply.github.com>
2026-02-05 01:44:21 +0900

    change fzf file search behaivior.

--- dot_config/private_fish/functions/fzf_file_select.fish
+++ dot_config/private_fish/functions/fzf_file_select.fish
@@ -1,4 +1,4 @@
-function fzf_file_select --description 'Select files with fzf, output relative paths to clipboard'
+function fzf_file_select --description 'Select files with fzf and output to stdout'
     # Get current path from argument (passed from tmux)
     set -l start_dir $argv[1]
     if test -z "$start_dir"
@@ -15,11 +15,12 @@ function fzf_file_select --description 'Select files with fzf, output relative p
     # List files with relative paths and select with fzf
     set -l selected (rg --files 2>/dev/null | \
         fzf -m \
+        --layout=reverse \
         --preview 'bat --color=always --line-range :100 {}' \
         --bind 'ctrl-a:select-all,ctrl-d:deselect-all')
 
     if test (count $selected) -gt 0
-        # Join with newlines and copy to clipboard
-        string join \n $selected | pbcopy
+        # Output to stdout (newline-separated)
+        string join \n $selected
     end
 end
